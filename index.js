086352cc9050671a141bda51bfb1d762355f7eef
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-06-30 11:29:56 -0400

    Added colors and JSON parsing

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 #! /usr/bin/env node
 
 const fs = require('fs')
+const colors = require('colors')
+
+colors.setTheme({
+    error: 'red',
+    data: 'green'
+})
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
-        console.log(err)
+        console.log(err.error)
     } else {
-        console.log(data)
-        return data
+        JSON.parse(data)
+        console.log(data.data)
+        return data.data
     }
 })
